ab6cf0810dfd4459fc9ef0c31c5921f71ed28368
Lucas Jan Malvin <113933847+Lucasjan3123@users.noreply.github.com>
2025-09-30 23:54:23 +0700

    Delete BusinessAdvisor directory

--- BusinessAdvisor/FinancialAdvisor.py
+++
@@ -1,9 +0,0 @@
-import streamlit as st
-
-st.markdown("# Financial Advisor Page  💰")
-st.sidebar.markdown("# Financial Advisor Page  💰")
-
-with st.form("financial_form"):
-    file =  st.file_uploader("Please upload your  financial statement", type=["pdf", "txt", "docx"])
-    goals = st.text_area("Enter your business goals (e.g., market leader, sustainable growth):")
-    submit_button = st.form_submit_button("analyze Financial Statement")
--- BusinessAdvisor/businessAdviceGenerator.py
+++
@@ -1,4 +0,0 @@
-import streamlit as st
-
-st.markdown("# Business Advice Generator Page  🧠")
-st.sidebar.markdown("# Business Advice Generator Page  🧠")
